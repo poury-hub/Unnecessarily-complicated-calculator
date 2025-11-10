c825cb080bd6c4d08e99697db0ea0486b5a5eb9e
poury <poury.2006.1385@gmail.com>
2025-11-11 01:12:41 +0330

    polished

--- do list/scri.js
+++ do list/scri.js
@@ -1,28 +1,28 @@
 let inputboy = document.getElementById('inp');
 let addboy = document.getElementById('but');
-let delboy = document.getElementsByClassName('del');
+//let delboy = document.getElementsByClassName('del');
 
 let cun = -1;
 
 addboy.onclick = function(){
     let thing = inputboy.value.trim();
+    inputboy.value = "";
+    inputboy.focus();
     if(thing === "") return;
-    console.log(thing);
+   
     cun++
-    let tclass='class'+cun;
+     console.log(cun);
     let t = document.createElement('tr');
-    t.className=tclass
     let n = document.createElement('td');
     n.textContent = cun + ":";
-    n.className=tclass
+    n.className="cun"
     let sub = document.createElement('td');
     sub.textContent = thing;
-    sub.className=tclass
     let d = document.createElement('td')
     let b = document.createElement('button');
     b.textContent="Delete"
-    b.className="delbut"
     d.append(b);
+
     t.append(n);
     t.append(sub);
     t.append(d);
@@ -31,10 +31,18 @@ addboy.onclick = function(){
     //tbody.append(t);
     tbody.insertBefore(t , addtr)
 
+    let allcuns = document.getElementsByClassName("cun");
 
     b.onclick = function() {
         if(confirm("are you sure?")){
-            t.remove()
+            let ind = t.rowIndex - 1;
+            console.log(ind);
+            t.remove();
+            for(let i = ind ; ind<allcuns.length ; i++){
+                allcuns[i].textContent = i + ":";
+            }
+            //cun = cin - 2
+            //console.log(cun);
         }
     }
 }
